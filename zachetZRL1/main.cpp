95cafb2291da433262dbb8e078b737aa9aaaa169
Vadim <vadim.fomin.2001@bk.ru>
2020-03-24 13:19:36 +0300

    Initial Commit

---
+++ zachetZRL1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  zachetZRL1
+//
+//  Created by Вадим on 24.03.2020.
+//  Copyright © 2020 1. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
